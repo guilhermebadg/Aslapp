c75b03991fe49cfd476beb4444e7ba7ce728fa4c
Guilherme Barbosa <102738385+guilhermebadg@users.noreply.github.com>
2022-07-11 20:42:10 -0300

    Update combos.js

--- js/combos.js
+++ js/combos.js
@@ -5,18 +5,3 @@ funcaobotao.addEventListener('click', () => {
  funcaobotao.style.display = 'none';
 
 })
-
-var btnleiamais = document.querySelector("#btnleiamais");
-var mais = document.querySelector("#mais");
-   
-btnleiamais.addEventListener('click', function() {
-   
-    if(mais.style.display === 'block'){
-       mais.style.display = 'none';
-       btnleiamais.innerHTML= 'Leia Mais';
-    }else{
-        mais.style.display = 'block';
-        btnleiamais.innerHTML= 'Leia Menos';
-    }
-
-});
